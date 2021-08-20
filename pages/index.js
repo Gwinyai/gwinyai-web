97b5555cb5353141903e8b26e3f00120e68d7cfc
Gwinyai <gnyatsoka@gmail.com>
2021-08-20 22:35:22 +0200

    Added rel

--- pages/index.js
+++ pages/index.js
@@ -29,10 +29,10 @@ export default function Home() {
             <h2 className={styles.subtitle}>iOS (Swift), NodeJS and React Developer</h2>
             <p className={styles.detail}>Gwinyai is CTO at <a href="https://cobbl.es">Cobbles LLC</a> based in Boston, Massachusetts. He is also a <a href="https://www.fingaz.co.zw/course-created-by-a-zimbabwean-course-attains-best-seller-status-on-udemy/">Udemy</a> best-selling iOS instructor with thousands of students across the globe.</p>
             <ul className={styles.socialList}>
-                <li><a href="https://www.facebook.com/paulthesage" target="_blank"><FontAwesomeIcon icon={faFacebook} size="2x"></FontAwesomeIcon></a></li>
-                <li><a href="https://www.linkedin.com/in/gwinyai-nyatsoka-4a026037/" target="_blank"><FontAwesomeIcon icon={faLinkedin} size="2x"></FontAwesomeIcon></a></li>
-                <li><a href="https://github.com/Gwinyai" target="_blank"><FontAwesomeIcon icon={faGithub} size="2x"></FontAwesomeIcon></a></li>
-                <li><a href="https://medium.com/@gwinyai" target="_blank"><FontAwesomeIcon icon={faMedium} size="2x"></FontAwesomeIcon></a></li>
+                <li><a href="https://www.facebook.com/paulthesage" target="_blank" rel="noopener noreferrer" ><FontAwesomeIcon icon={faFacebook} size="2x"></FontAwesomeIcon></a></li>
+                <li><a href="https://www.linkedin.com/in/gwinyai-nyatsoka-4a026037/" target="_blank" rel="noopener noreferrer" ><FontAwesomeIcon icon={faLinkedin} size="2x"></FontAwesomeIcon></a></li>
+                <li><a href="https://github.com/Gwinyai" target="_blank" rel="noopener noreferrer" ><FontAwesomeIcon icon={faGithub} size="2x"></FontAwesomeIcon></a></li>
+                <li><a href="https://medium.com/@gwinyai" target="_blank" rel="noopener noreferrer" ><FontAwesomeIcon icon={faMedium} size="2x"></FontAwesomeIcon></a></li>
             </ul>
             <div className={styles.buttons}> <button onClick={handleContactAction} className="btn btn-outline-primary px-4">Contact</button> </div>
       </main>
