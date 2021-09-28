7279d7ac2ea00a1a050cb522b7332777ef7971ee
Gwinyai <gnyatsoka@gmail.com>
2021-09-29 00:30:11 +0200

    Typo fixes and application loading state changes

--- apollo-client.js
+++ apollo-client.js
@@ -1,8 +1,14 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
+let uri = "https://gwinyai-dev-api.herokuapp.com/graphql";
+
+if (process.env.ENV == "PROD") {
+  uri = "https://gwinyai-api.herokuapp.com/graphql";
+}
+
 const client = new ApolloClient({
-    uri: "https://gwinyai-api.herokuapp.com/graphql",
-    cache: new InMemoryCache(),
+  uri,
+  cache: new InMemoryCache(),
 });
 
 export default client;
--- pages/course-application.js
+++ pages/course-application.js
@@ -1,6 +1,10 @@
 import styles from "../styles/Application.module.css";
+import Head from "next/head";
+import Router from "next/router";
 import { useState } from "react";
+import { Alert } from "react-bootstrap";
 import { useMutation, gql } from "@apollo/client";
+import classnames from "classnames";
 
 const CREATE_PROSPECT = gql`
   mutation CreateProspect(
@@ -41,11 +45,12 @@ const Application = () => {
   const [courseError, setCourseError] = useState("");
   const [skill, setSkill] = useState("BEGINNER");
   const [availability, setAvailability] = useState("MORNING");
+  const [showAlert, setShowAlert] = useState(false);
 
   const [createProspect, { loading: mutationLoading, error: mutationError }] =
     useMutation(CREATE_PROSPECT, {
       onCompleted: (data) => {
-        console.log("successful submission!");
+        setShowAlert(true);
       },
       onError: (error) => {
         console.log(error.message);
@@ -164,142 +169,195 @@ const Application = () => {
     setAvailability(e.target.value);
   };
 
-  return (
-    <div className="jumbotron verticalCenter">
-      <div className="container">
-        <h1 className={styles.title}>Course Application</h1>
-        <p className={styles.detail}>
-          When you submit this application, an automated message with your
-          student account login details will be sent via email and SMS. Use your
-          student account to make payment and enroll in the course.
-        </p>
-        {mutationError && (
-          <div className="formError">{mutationError.message}</div>
-        )}
-        <form onSubmit={onSubmit} className="form">
-          <div className="form-group">
-            <label htmlFor="firstname">First Name:</label>
-            <input
-              name="firstname"
-              className={`form-control`}
-              placeholder="Enter First Name"
-              id="firstname"
-              autoFocus
-              onChange={handleFirstNameChange}
-            />
-            <div className="formError">{firstNameError}</div>
-          </div>
-          <div className="form-group">
-            <label htmlFor="lastname">Last Name:</label>
-            <input
-              name="lastname"
-              className={`form-control`}
-              placeholder="Enter Last Name"
-              onChange={handleLastNameChange}
-              id="lastname"
-            />
-            <div className="formError">{lastNameError}</div>
-          </div>
-          <div className="form-group">
-            <label htmlFor="email">Email:</label>
-            <input
-              type="email"
-              name="email"
-              className={`form-control`}
-              placeholder="Enter Email"
-              id="email"
-              onChange={handleEmailChange}
-            />
-            <div className="formError">{emailError}</div>
-          </div>
-          <div className="form-group">
-            <label htmlFor="phone">Phone Number:</label>
-            <div className="input-group">
-              <div
-                className="input-group-prepend"
-                style={{ margin: "15px 0 0 0" }}
-              >
-                <span className="input-group-text">+263</span>
-              </div>
-              <input
-                name="phone"
-                className={`form-control`}
-                placeholder="Enter Phone Number"
-                id="phone"
-                onChange={handlePhoneChange}
-              />
-            </div>
-            <div className="formError">{phoneError}</div>
-          </div>
-          <div className="form-group">
-            <label>Select Courses:</label>
-            <div className={styles.checkboxSection}>
-              <div className="form-check form-check-inline">
-                <input
-                  className="form-check-input"
-                  type="checkbox"
-                  id="nodejs"
-                  value="nodejs"
-                  onChange={handleNodeChange}
-                />
-                <label className="form-check-label" htmlFor="nodejs">
-                  NodeJS
-                </label>
-              </div>
-              <div className="form-check form-check-inline">
-                <input
-                  className="form-check-input"
-                  type="checkbox"
-                  id="reactjs"
-                  value="reactjs"
-                  onChange={handleReactChange}
-                />
-                <label className="form-check-label" htmlFor="nodejs">
-                  ReactJS
-                </label>
-              </div>
-            </div>
-            <div className="formError">{courseError}</div>
-          </div>
-          <div className="form-group">
-            <label htmlFor="skill">Your Skill Level with Javascript:</label>
-            <select
-              className="form-control"
-              id="skill"
-              name="skill"
-              value={skill}
-              onChange={handleSkillChange}
-            >
-              <option value="BEGINNER">Beginner</option>
-              <option value="INTERMEDIATE">Intermediate</option>
-              <option value="PROFESSIONAL">Professional</option>
-            </select>
-          </div>
-          <div className="form-group">
-            <label htmlFor="time">Your Preferred Time for Lessons:</label>
-            <select
-              className="form-control"
-              id="time"
-              name="time"
-              value={availability}
-              onChange={handleAvailabilityChange}
-            >
-              <option value="MORNING">Morning</option>
-              <option value="AFTERNOON">Afternoon</option>
-              <option value="EVENING">Evening</option>
-            </select>
-          </div>
-          <div className="d-grid gap-2">
-            <button
-              type="submit"
-              className={["btn", "btn-primary", styles.submitButton].join(" ")}
-            >
-              Apply
-            </button>
-          </div>
-        </form>
+  if (showAlert) {
+    return (
+      <div className="jumbotron verticalCenter">
+        <div className="container">
+          <Alert
+            variant="success"
+            onClose={() => {
+              Router.push("/");
+            }}
+            dismissible
+          >
+            <Alert.Heading>Success!</Alert.Heading>
+            <p>
+              Proceed to{" "}
+              <a href="https://student.gwinyai.com">
+                https://student.gwinyai.com
+              </a>{" "}
+              to enroll in the course and make payment. Your password has been
+              sent to the email and phone number you provided in the
+              application.
+            </p>
+          </Alert>
+        </div>
       </div>
-    </div>
+    );
+  }
+
+  return (
+    <>
+      <Head>
+        <title>Course Application</title>
+        <meta
+          name="description"
+          content="Apply to the NodeJS and ReactJS master course."
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className="jumbotron verticalCenter">
+        <div className="container">
+          <h1 className={styles.title}>Course Application</h1>
+          <p className={styles.detail}>
+            When you submit this application, an automated message with your
+            student account login details will be sent via email and SMS. Use
+            your student account to make payment and enroll in the course.
+          </p>
+          {mutationError && (
+            <div className="formError">{mutationError.message}</div>
+          )}
+          <form onSubmit={onSubmit} className="form">
+            <div className="form-group">
+              <label htmlFor="firstname">First Name:</label>
+              <input
+                name="firstname"
+                className={`form-control`}
+                placeholder="Enter First Name"
+                id="firstname"
+                autoFocus
+                onChange={handleFirstNameChange}
+              />
+              <div className="formError">{firstNameError}</div>
+            </div>
+            <div className="form-group">
+              <label htmlFor="lastname">Last Name:</label>
+              <input
+                name="lastname"
+                className={`form-control`}
+                placeholder="Enter Last Name"
+                onChange={handleLastNameChange}
+                id="lastname"
+              />
+              <div className="formError">{lastNameError}</div>
+            </div>
+            <div className="form-group">
+              <label htmlFor="email">Email:</label>
+              <input
+                type="email"
+                name="email"
+                className={`form-control`}
+                placeholder="Enter Email"
+                id="email"
+                onChange={handleEmailChange}
+              />
+              <div className="formError">{emailError}</div>
+            </div>
+            <div className="form-group">
+              <label htmlFor="phone">Phone Number:</label>
+              <div className="input-group">
+                <div
+                  className="input-group-prepend"
+                  style={{ margin: "15px 0 0 0" }}
+                >
+                  <span className="input-group-text">+263</span>
+                </div>
+                <input
+                  name="phone"
+                  className={`form-control`}
+                  placeholder="Enter Phone Number"
+                  id="phone"
+                  onChange={handlePhoneChange}
+                />
+              </div>
+              <div className="formError">{phoneError}</div>
+            </div>
+            <div className="form-group">
+              <label>Select Courses:</label>
+              <div className={styles.checkboxSection}>
+                <div className="form-check form-check-inline">
+                  <input
+                    className="form-check-input"
+                    type="checkbox"
+                    id="nodejs"
+                    value="nodejs"
+                    onChange={handleNodeChange}
+                  />
+                  <label className="form-check-label" htmlFor="nodejs">
+                    NodeJS
+                  </label>
+                </div>
+                <div className="form-check form-check-inline">
+                  <input
+                    className="form-check-input"
+                    type="checkbox"
+                    id="reactjs"
+                    value="reactjs"
+                    onChange={handleReactChange}
+                  />
+                  <label className="form-check-label" htmlFor="nodejs">
+                    ReactJS
+                  </label>
+                </div>
+              </div>
+              <div className="formError">{courseError}</div>
+            </div>
+            <div className="form-group">
+              <label htmlFor="skill">Your Skill Level with Javascript:</label>
+              <select
+                className="form-control"
+                id="skill"
+                name="skill"
+                value={skill}
+                onChange={handleSkillChange}
+              >
+                <option value="BEGINNER">Beginner</option>
+                <option value="INTERMEDIATE">Intermediate</option>
+                <option value="PROFESSIONAL">Professional</option>
+              </select>
+            </div>
+            <div className="form-group">
+              <label htmlFor="time">Your Preferred Time for Lessons:</label>
+              <select
+                className="form-control"
+                id="time"
+                name="time"
+                value={availability}
+                onChange={handleAvailabilityChange}
+              >
+                <option value="MORNING">Morning</option>
+                <option value="AFTERNOON">Afternoon</option>
+                <option value="EVENING">Evening</option>
+              </select>
+            </div>
+            <div className="d-grid gap-2">
+              <button
+                type="submit"
+                className={classnames(
+                  "btn",
+                  "btn-primary",
+                  styles.submitButton
+                )}
+                disabled={mutationLoading}
+              >
+                {mutationLoading ? (
+                  <>
+                    <span
+                      className="spinner-grow spinner-grow-sm"
+                      role="status"
+                      aria-hidden="true"
+                    ></span>
+                    &nbsp;&nbsp;<span className="sr-only">Loading...</span>
+                  </>
+                ) : (
+                  "Submit"
+                )}
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </>
   );
 };
 
