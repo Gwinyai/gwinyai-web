23f3b3bcc6f77484233fe7d60194beb8ba174967
Gwinyai <gnyatsoka@gmail.com>
2021-09-29 00:51:41 +0200

    hardcoded uri

--- apollo-client.js
+++ apollo-client.js
@@ -1,13 +1,7 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
-let uri = "https://gwinyai-dev-api.herokuapp.com/graphql";
-
-if (process.env.ENV == "PROD") {
-  uri = "https://gwinyai-api.herokuapp.com/graphql";
-}
-
 const client = new ApolloClient({
-  uri,
+  uri: "https://gwinyai-api.herokuapp.com/graphql",
   cache: new InMemoryCache(),
 });
 
