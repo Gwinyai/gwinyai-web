c779cf722d5a9240d0fa2674bd0a8a72d5a0162b
Gwinyai <gnyatsoka@gmail.com>
2021-11-12 21:50:42 +0200

    Mobile responsiveness and mobile navigation

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import AppRouter from "./router/AppRouter";
 import "./styles/styles.css";
+import "./styles/queries.css";
 
 function App() {
   return (
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,38 +1,72 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTimes, faBars } from "@fortawesome/free-solid-svg-icons";
 
 export default function Navigation() {
+  const [navOpen, setNav] = useState(false);
+  const showNav = () => {
+    setNav(true);
+  };
+  const closeNav = () => {
+    setNav(false);
+  };
   return (
     <>
-      <header className="header">
+      <header className={navOpen ? "nav-open header" : "header"}>
         <nav className="main-nav">
           <ul className="main-nav-list">
             <li>
-              <Link to="/" className="main-nav-link">
+              <Link onClick={closeNav} to="/" className="main-nav-link">
                 Home
               </Link>
             </li>
             <li>
-              <Link to="/skills" className="main-nav-link">
+              <Link onClick={closeNav} to="/skills" className="main-nav-link">
                 Skills
               </Link>
             </li>
             <li>
-              <Link to="/portfolio" className="main-nav-link">
+              <Link
+                onClick={closeNav}
+                to="/portfolio"
+                className="main-nav-link"
+              >
                 Portfolio
               </Link>
             </li>
             <li>
-              <Link to="/courses" className="main-nav-link">
+              <Link onClick={closeNav} to="/courses" className="main-nav-link">
                 Courses
               </Link>
             </li>
             <li>
-              <Link to="/experience" className="main-nav-link">
+              <Link
+                onClick={closeNav}
+                to="/experience"
+                className="main-nav-link"
+              >
                 Experience
               </Link>
             </li>
           </ul>
         </nav>
+        <button className="btn-mobile-nav">
+          <FontAwesomeIcon
+            className="icon-mobile-nav"
+            name="close-outline"
+            icon={faTimes}
+            size="2x"
+            onClick={closeNav}
+          ></FontAwesomeIcon>
+          <FontAwesomeIcon
+            className="icon-mobile-nav"
+            name="menu-outline"
+            icon={faBars}
+            size="2x"
+            onClick={showNav}
+          ></FontAwesomeIcon>
+        </button>
       </header>
     </>
   );
